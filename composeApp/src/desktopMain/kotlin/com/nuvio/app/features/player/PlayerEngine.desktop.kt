f587cc5a18598b1331a7efa482ce4f3e4551edeb
ahmed <ahmed@example.com>
2026-06-21 17:17:50 -0400

    Add RPM tasks to linuxNativePlayerTasks, rpmLicenseType, and noteCursorActivity for Linux

--- composeApp/src/desktopMain/kotlin/com/nuvio/app/features/player/PlayerEngine.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/nuvio/app/features/player/PlayerEngine.desktop.kt
@@ -220,6 +220,7 @@ private fun LinuxComposeSurface(
         val listener = AWTEventListener { awtEvent ->
             when (awtEvent.id) {
                 MouseEvent.MOUSE_MOVED, MouseEvent.MOUSE_DRAGGED -> {
+                    host.noteCursorActivity()
                     latestOnPlayerControlsEvent.value("keepChromeVisible", 0.0)
                 }
                 MouseEvent.MOUSE_PRESSED -> {
